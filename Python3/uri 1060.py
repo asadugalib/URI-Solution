2713ff5bd4c0bb55455d14baf05104b8b22d8e21
ASAD <61794969+asadugalib@users.noreply.github.com>
2021-03-30 20:43:27 +0600

    Update uri 1060.py

--- Python3/uri 1060.py
+++ Python3/uri 1060.py
@@ -6,10 +6,9 @@ pos=0
 for i in range(6):
     number.append(float(input()))
 
-for i in range(len(number)):
-    test = 0+number[i]
-    if test > 0:
+for i in number:
+    if i > 0:
         pos+=1
-    test = 0
+
     
 print("{} valores positivos".format(pos))
